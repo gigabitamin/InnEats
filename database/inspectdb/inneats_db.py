5ee026b74cec9aa633c8d1e5607cce7d32858265
gigabitamin <gigabitamin@gmail.com>
2023-10-17 23:29:19 +0900

    v0.2.0

--- database/inspectdb/inneats_db.py
+++ database/inspectdb/inneats_db.py
@@ -1,6 +1,36 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
 class DailyHotel(models.Model):
     daily_hotel_name = models.CharField(primary_key=True, max_length=200)  # The composite primary key (daily_hotel_name, daily_hotel_address) found, that is not supported. The first column is selected.
     daily_hotel_address = models.CharField(max_length=255)
@@ -14,6 +44,52 @@ class DailyHotel(models.Model):
         managed = False
         db_table = 'daily_hotel'
         unique_together = (('daily_hotel_name', 'daily_hotel_address'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey('UsersAppUser', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
 
 class Goodchoice(models.Model):
     acc_name = models.CharField(primary_key=True, max_length=200)  # The composite primary key (acc_name, acc_address) found, that is not supported. The first column is selected.
@@ -93,6 +169,7 @@ class Restaurant(models.Model):
         managed = False
         db_table = 'restaurant'
 
+
 class Tripbtoz(models.Model):
     trip_name = models.CharField(primary_key=True, max_length=200)  # The composite primary key (trip_name, trip_address) found, that is not supported. The first column is selected.
     trip_address = models.CharField(max_length=255)
@@ -107,6 +184,49 @@ class Tripbtoz(models.Model):
         db_table = 'tripbtoz'
         unique_together = (('trip_name', 'trip_address'),)
 
+
+
+class UsersAppUser(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+    user_name = models.CharField(max_length=30)
+    user_phone = models.CharField(max_length=20)
+    user_address = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'users_app_user'
+
+
+class UsersAppUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(UsersAppUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'users_app_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class UsersAppUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(UsersAppUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'users_app_user_user_permissions'
+        unique_together = (('user', 'permission'),)
 
 
 class Visitkorea(models.Model):
@@ -155,6 +275,3 @@ class Youtube(models.Model):
     class Meta:
         managed = False
         db_table = 'youtube'
-
-
-
