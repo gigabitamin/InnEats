ed9f7d26a60834f085366485b0ddd94b32022646
bugneet <bugneet.dev@gmail.com>
2023-10-20 15:26:16 +0900

    model의 visitkorea 자료형 수정, accommodation_app view 수정 및 visitkorea DB 연동

--- accommodation_app/urls.py
+++ accommodation_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('accommodation/', views.accommodation, name='accommodation'),
-    path('accommodation_detail/', views.accommodation_detail, name='accommodation_detail'),
+    path('accommodation/<str:keyword>', views.accommodation, name='accommodation'),
+    path('accommodation_detail/<str:keyword>', views.accommodation_detail, name='accommodation_detail'),
 ]
