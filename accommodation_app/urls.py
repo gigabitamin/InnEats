98ddbe91ca34db993a049dc0932d56d33b0ac28b
gigabitamin <gigabitamin@gmail.com>
2023-10-22 02:41:40 +0900

    231022

--- accommodation_app/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('accommodation/<str:keyword>', views.accommodation, name='accommodation'),
-    path('accommodation_detail/<str:keyword>', views.accommodation_detail, name='accommodation_detail'),
-]
