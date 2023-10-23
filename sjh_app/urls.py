9d11390d37427e424f54877174e7fdd63b0658c6
sdoohee <sdoohee0808@naver.com>
2023-10-23 17:38:52 +0900

    sjh_app map update

--- sjh_app/urls.py
+++ sjh_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('sjh_app/', views.sjh_app, name='sjh_app'),
+    path('display_map/', views.display_map, name='display_map'),
 ]
--- sjh_app/views.py
+++ sjh_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def sjh_app(request):
     return render(request, 'sjh_app/map_detail.html')
+
+def display_map(request):
+    return render(request, 'sjh_app/map.html')
