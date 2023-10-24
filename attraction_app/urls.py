58b1f327463ea390e4a2602be8b3c0e6533e41de
Min Seok Suh <samseung17@naver.com>
2023-10-24 15:59:11 +0900

    여행지 페이지 추가

--- attraction_app/urls.py
+++ attraction_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('attraction/', views.attraction, name='attraction'),
+    path('attraction/<str:keyword>', views.attraction, name='attraction'),
+    
 ]
