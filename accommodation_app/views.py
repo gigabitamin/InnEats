699b1a06d5294e5b62da5db0538c8cc5d832e43d
bugneet <bugneet.dev@gmail.com>
2023-10-25 14:09:57 +0900

    add search bar in accommodation

--- accommodation_app/views.py
+++ accommodation_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.db.models import Q
 from inneats_app.models import Visitkorea
-from kdy_app.models import DailyHotel 
 from inneats_app.models import Youtube
+from inneats_app.models import Accommodation
 
 # Create your views here.
 def accommodation(request,keyword):    
@@ -10,7 +10,7 @@ def accommodation(request,keyword):
     # 우도면
     # q_objects = Q(visitkorea_title__contains=keyword)
 
-    accommodation_list = DailyHotel.objects.filter(Q(daily_hotel_address__contains=keyword))
+    accommodation_list = Accommodation.objects.filter(Q(address__contains=keyword))
     return render(request, 'accommodation_app/accommodation.html',{'keyword':keyword, 'accommodation_list':accommodation_list})
 
 
@@ -18,8 +18,8 @@ def accommodation_detail(request, accommodation_id):
 
     # 하위 주소를 토대로 관광지 리스트 쿼리 
     # addr = "제주 서귀포시 안덕면"
-    accommodation = DailyHotel.objects.get(Q(daily_hotel_num=accommodation_id))
-    keywords = accommodation.daily_hotel_address.split(" ")
+    accommodation = Accommodation.objects.get(Q(id=accommodation_id))
+    keywords = accommodation.address.split(" ")
 
     if len(keywords) > 2 :
         keyword = keywords[2]
@@ -33,19 +33,23 @@ def accommodation_detail(request, accommodation_id):
 
 
 
-    youtube_list = Youtube.objects.filter(Q(visitkorea_address__contains=keywords))
+    # youtube_list = Youtube.objects.filter(Q(visitkorea_address__contains=keywords))
 
     # 숙소 주변 관광지 리스트를 토대로 쿼리 날리는 방법 #
     #################################################################################
     # 키워드를 포함한 리스트
     keywords = [attr.visitkorea_title for attr in attraction_list]  # 나머지 키워드들을 포함
     # 초기 쿼리 생성
-    q_objects = Q(visitkorea_title__contains=keywords[0])
+
+    # for keyword in keywords:
+    #     print(keyword)
+    
+    q_objects = Q(youtube_title__contains=keywords[0])
     # 나머지 키워드들을 OR 조건으로 추가
     for keyword in keywords[1:]:
-        q_objects |= Q(visitkorea_title__contains=keyword)
+        q_objects |= Q(youtube_title__contains=keyword)
     # 쿼리 실행
-    youtube_list = Visitkorea.objects.filter(q_objects)
+    youtube_list = Youtube.objects.filter(q_objects)
     #################################################################################
 
     attraction_list = attraction_list[:3]
